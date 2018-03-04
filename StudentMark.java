4fb98e5c05dcbb4bbe0e410b6b2b5c2a24faec03
unknown <pbgaya1993@gmail.com>
2018-03-04 12:08:55 +0530

    find avarage ot the sutudent marks

--- StudentMark.java
+++ StudentMark.java
@@ -4,13 +4,14 @@ class StudentMark{
      int mark[]=new int[10];
      input_mark(mark);
      total(mark);
+     avarage(mark);
   }
   
   public static void input_mark(int mark[]){
 	  Scanner input=new Scanner(System.in);
 	  System.out.println("Input student Marks");
 	  for(int i=0;i<mark.length;i++){
-			 System.out.print("Student "+(i+1)+" : ");
+			 System.out.print("Student "+(i+1)+"\t: ");
 			 mark[i]=input.nextInt();
 
 	  }
@@ -25,8 +26,20 @@ class StudentMark{
 		  sum+=mark[i];
 		  
 		  }
-	  System.out.println("Totalt fo the Sudents Mark :"+sum);
+	  System.out.println("Totalt fo the Sudents Mark\t:"+sum);
 	  }
 
+ 
+  public static void avarage(int mark[]){
+	  int sum=0;
+	  int avg=0;
+	  for(int i=0;i<mark.length;i++){
+		  sum+=mark[i];
+		  
+		  }
+		
+	  avg=sum/mark.length;
+	  System.out.println("Avarage fo the Sudents Mark\t:"+avg);
+	  }
 
 }
